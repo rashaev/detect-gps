fae03835cfce8929a9e14627430f49cfca0dfcae
Roman Ashaev <rashaev@yandex.ru>
2020-05-17 19:15:18 +0300

    added command flags
    Added flags to specify baudrate and timeout

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/adrianmo/go-nmea v1.2.0
 	github.com/creack/goselect v0.1.1 // indirect
+	github.com/spf13/pflag v1.0.5
 	go.bug.st/serial.v1 v0.0.0-20191202182710-24a6610f0541
 	golang.org/x/sys v0.0.0-20200513112337-417ce2331b5c // indirect
 )
--- src/detect-gps.go
+++ src/detect-gps.go
@@ -1,60 +1,75 @@
 package main
 
 import (
-	"go.bug.st/serial.v1"
-	"github.com/adrianmo/go-nmea"
-	"fmt"
 	"bufio"
+	"fmt"
 	"log"
 	"regexp"
 	"time"
+
+	"github.com/adrianmo/go-nmea"
+	flag "github.com/spf13/pflag"
+	"go.bug.st/serial.v1"
 )
 
+var baudrate int
+var timeOut time.Duration
+
+func init() {
+	flag.IntVarP(&baudrate, "baudrate", "b", 115200, "baud rate")
+	flag.DurationVarP(&timeOut, "timeout", "t", 5s*time.Second, "max time in seconds to read data from serial port")
+}
 
 func readPort(ttyport string, timeout time.Duration) string {
 	mode := &serial.Mode{
-		BaudRate: 115200,
+		BaudRate: baudrate,
 	}
 
 	chResult := make(chan string, 1)
-	
+
 	port, err := serial.Open(ttyport, mode)
-		if err != nil {
-			return ""
-		}
+	if err != nil {
+		return ""
+	}
+
 	defer port.Close()
 
 	regexGps := regexp.MustCompile(`GNRMC`)
 
-	go func(){
+	go func() {
 		for {
-			s, _ :=  bufio.NewReader(port).ReadString('\n')
-			newstr := regexGps.FindString(s)
-			if newstr != "" {
-				sentence, err := nmea.Parse(s)
-				if err == nil {
-					if sentence.DataType() == nmea.TypeRMC {
-						m := sentence.(nmea.RMC)
-						chResult <- fmt.Sprintf("Latitude:%s;Longitude:%s;%s", nmea.FormatGPS(m.Latitude), nmea.FormatGPS(m.Longitude), ttyport)
-						break
+			reader := bufio.NewReader(port)
+			scanner := bufio.NewScanner(reader)
+			for scanner.Scan() {
+				nextString := scanner.Text()
+
+				newstr := regexGps.FindString(nextString)
+				if newstr != "" {
+					sentence, err := nmea.Parse(nextString)
+					if err == nil {
+						if sentence.DataType() == nmea.TypeRMC {
+							m := sentence.(nmea.RMC)
+							chResult <- fmt.Sprintf("Latitude:%s;Longitude:%s;%s", nmea.FormatGPS(m.Latitude), nmea.FormatGPS(m.Longitude), ttyport)
+							break
 						}
-				}			
-			}	
+					}
+				}
+			}
 		}
 	}()
 
 	select {
-	case data := <- chResult:
+	case data := <-chResult:
 		return data
 	case <-time.After(timeout):
 		return ""
 	}
-		
 }
 
-func main(){
+func main() {
+	flag.Parse()
+
 	var ttySerial []string
-	
 
 	ports, err := serial.GetPortsList()
 	if err != nil {
@@ -68,12 +83,12 @@ func main(){
 
 	for _, port := range ports {
 		if re.MatchString(port) == true {
-		ttySerial = append(ttySerial, port)
+			ttySerial = append(ttySerial, port)
 		}
 	}
 
 	for _, ttyport := range ttySerial {
-		strRes := readPort(ttyport, 3 * time.Second)
+		strRes := readPort(ttyport, timeOut)
 		if strRes != "" {
 			fmt.Println(strRes)
 			break
